c432067222cd3c6e5b7a786e126a466d59a799d3
Jake Turner <jakelturner@gmail.com>
2014-04-06 08:09:15 +1000

    * Changed shop building detecting to on button instead of onbuttondown

--- Assets/scripts/InputController.js
+++ Assets/scripts/InputController.js
@@ -2,19 +2,15 @@
 
 private static var Instance : InputController = null;
 var _mask : LayerMask;
+private var _prevTile : Tile;
 
 public static function Get() : InputController
-
 {
     return Instance;
 }
 
 public function InputController()
 {
-    //if the constructor must be public, you can do this:
-    if (Instance != null)
-    {
-    }
 }
 
 function Awake()
@@ -22,19 +18,19 @@ function Awake()
     Instance = this;
 }
 
-function Start () {
-
-}
-
 function Update () {
-	if (Input.GetMouseButtonDown(1)) {
+	if (Input.GetMouseButton(0)) {
 		var ray : Ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 	    var hit : RaycastHit2D = Physics2D.GetRayIntersection(ray, 100, _mask);
 	    
 	    if (hit && hit.transform.tag == "Tile") {
 	    	var tile : Tile = hit.transform.GetComponent("Tile") as Tile;
-	    	
-	    	BuildManager.Get().CheckBuildPosition(tile);
+	    	if(_prevTile == null || _prevTile != tile) { 
+				_prevTile = tile;
+	    		BuildManager.Get().CheckBuildPosition(tile);
+	    	} else {
+    			Debug.Log("checking");
+    		}
 	    }
 	}
 }
