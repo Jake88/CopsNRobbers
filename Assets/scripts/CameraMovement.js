e5cfffe6c7a28e77394108a4d95a4497ba20e439
JAKE TURNER <jakelturner@gmail.com>
2014-04-08 19:33:00 +1000

    *Added camera movement (not tested for android) * WIP still no boundries stopping camera from moving away from game grid.

---
+++ Assets/scripts/CameraMovement.js
@@ -0,0 +1,55 @@
+﻿#pragma strict
+ 
+private var _dragOrigin: Vector3;
+private var _velocity : float = 0;
+private var _touchDevice : boolean;
+private var pos : Vector3;
+var _maxVelocity : float;
+
+function Start() {
+	if(Input.mousePresent) {
+		_touchDevice = false;
+	} else {
+		_touchDevice = true;
+	}
+}
+
+function Update() {
+	if(_touchDevice) {
+		if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved) {
+		    pos = Input.GetTouch(0).deltaPosition;
+		    IncreaseVelocity();
+		} else {
+			DecreaseVelocity();
+		}
+	} else {
+		if (Input.GetMouseButtonDown(0)){
+			_dragOrigin = Input.mousePosition;
+		    return;
+	    }
+	    if (Input.GetMouseButton(0)) {
+			IncreaseVelocity();
+			pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - _dragOrigin);
+		} else {
+			DecreaseVelocity();
+		}
+	}
+	
+	if (_velocity > 0) {
+		transform.Translate(-pos.x * _velocity, -pos.y * _velocity, 0); 
+	}
+}
+
+private function IncreaseVelocity() {
+	_velocity += Time.deltaTime;
+	if (_velocity > _maxVelocity) {
+		_velocity = _maxVelocity;
+	}
+}
+
+private function DecreaseVelocity() {
+	_velocity -= Time.deltaTime;
+	if (_velocity < 0) {
+		_velocity = 0;
+	}
+}
